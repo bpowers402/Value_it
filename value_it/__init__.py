397ad9a86d1dc9a84696f6a3ffaed8d35b09ac5d
bpowers402 <billie-jopowers@outlook.com>
2025-03-26 08:33:57 +0000

    Add files via upload

---
+++ value_it/__init__.py
@@ -0,0 +1,4 @@
+from .value_it import value_it
+
+__version__ = '1.0.0'
+__author__ = 'Billie-Jo Powers'
---
+++ value_it/value_it.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def value_it(S,A,P,R,I) :
+    #S is the set of all states
+    #A is the set of all actions
+    #P is the state transition function, which will be an sxsxa matrix
+    #R is a reward function R(s,a) so is always 2 dimensional
+    #I is number of iterations they want
+    s = len(S) #number of states
+    a = len(A) #number of actions
+    gamma = 0.8
+    V = np.zeros((I+1, s))
+    pi = [0]*s
+    for k in range(1, (I+1)):
+        arg_max = [0]*s 
+        for i in range(0,s):
+            action_vec = [0]*a
+            for j in range(0,a):
+                sum_vec = [0]*s
+                for n in range(0,s):
+                    sum_vec[n] = P[n,i,j]*V[k-1,n]
+                action_vec[j] = R[i,j] + gamma*sum(sum_vec)
+            V[k,i] = max(action_vec)
+            arg_max[i] = np.argmax(action_vec)
+            pi[i] = A[arg_max[i]]
+    print(V[(k),])
+    return (V[(k),], pi)
