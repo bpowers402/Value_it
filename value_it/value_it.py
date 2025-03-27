4a8d695854f8e20a6e22e0e64aff486ba92aa5ee
bpowers402 <billie-jopowers@outlook.com>
2025-03-27 10:09:50 +0000

    Update value_it.py
    updating variables in the actual code

--- value_it/value_it.py
+++ value_it/value_it.py
@@ -7,8 +7,8 @@ def value_it(States, Actions, Probs, Rewards, I = 1000, gamma = 0.8) :
     #Rewards is a reward function R(s,a) so is always 2 dimensional
     #I is number of iterations they want
     #gamma in the discount 
-    s = len(S) #number of states
-    a = len(A) #number of actions
+    s = len(States) #number of states
+    a = len(Actions) #number of actions
     V = np.zeros((I+1, s))
     pi = [0]*s
     for k in range(1, (I+1)):
@@ -18,9 +18,9 @@ def value_it(States, Actions, Probs, Rewards, I = 1000, gamma = 0.8) :
             for j in range(0,a):
                 sum_vec = [0]*s
                 for n in range(0,s):
-                    sum_vec[n] = P[n,i,j]*V[k-1,n]
-                action_vec[j] = R[i,j] + gamma*sum(sum_vec)
+                    sum_vec[n] = Probs[n,i,j]*V[k-1,n]
+                action_vec[j] = Rewards[i,j] + gamma*sum(sum_vec)
             V[k,i] = max(action_vec)
             arg_max[i] = np.argmax(action_vec)
-            pi[i] = A[arg_max[i]]
+            pi[i] = Actions[arg_max[i]]
     return (V[(k),], pi)
